99268e3afab5a59dc49545771e187a9b78d9ef99
Henry Bergström <henrybergstrom@protonmail.com>
2022-10-11 10:10:42 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const log = (message) => console.log(`[CLIENT] ${message}`);
 
 function init(e) {
-  const websocket = new WebSocket("ws://localhost:3000");
+  const websocket = new WebSocket(`ws://${window.location.href.split("//")[1]}:3000`);
   const canvas = document.querySelector("#canvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
